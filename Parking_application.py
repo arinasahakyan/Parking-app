0763f7a62f75d2c1d9090f4286a41f82e2e645b1
Artashes-Voskanyan <62055368+Artashes-Voskanyan@users.noreply.github.com>
2020-03-11 14:17:14 +0400

    Update Parking_application.py

--- Parking_application.py
+++ Parking_application.py
@@ -1,1 +1,16 @@
+ suv = input ("is your car suv?")
+if (suv=="no"):
 
+  if (car_type=="suv"):
+    sedan = input("is your car sedan")
+    if (sedan=="yes"):
+
+      available = input("available space?")
+      if (available=="no"):
+
+        print ("Redlight")
+       else
+        print ("Greenlight")
+     else print ("End")
+   else print ("end")
+else print ("end")
