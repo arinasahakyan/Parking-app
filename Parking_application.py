a33c3c22be319220637133e4f9e4796e4da0e1e5
yevamelikyan <62141135+yevamelikyan@users.noreply.github.com>
2020-03-25 14:43:23 +0400

    Update Parking_application.py

--- Parking_application.py
+++ Parking_application.py
@@ -1,16 +1,13 @@
- suv = input ("is your car suv?")
-if (suv=="no"):
+ import random #use Python random package to get random numbers
 
-  if (car_type=="suv"):
-    sedan = input("is your car sedan")
-    if (sedan=="yes"):
+carType = ["sedan", "suv"] #store two possible values for answer
+answer=0
 
-      available = input("available space?")
-      if (available=="no"):
-
-        print ("Redlight")
-       else
-        print ("Greenlight")
-     else print ("End")
-   else print ("end")
-else print ("end")
+while carType[0] != answer and carType[1] != answer:
+  answer = input("Please choose the type of your car: sedan or suv? ")
+if answer == carType[0]:
+    print("Your parking spot is: ", random.randrange(1, 101, 1)) #prints random parking spot number  between 1 and 100 for car type chosen as sedan
+elif answer == carType[1]:
+    print("Your parking spot is: ", random.randrange(101, 201, 1)) #prints random parking spot number  between 101 and 200 for car type chosen as SUV
+else:
+    print("Type chosen incorrectly") 
